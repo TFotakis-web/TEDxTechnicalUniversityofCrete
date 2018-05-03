d8c1cb221b9ad21d9fb155486e2c7c7df25c93b3
TFotakis <fotakistzanis@gmail.com>
2018-05-03 20:49:15 +0300

    Made tickets counter crawler sign in and get its own cookie every 10 hours

--- TEDx2018/views.py
+++ TEDx2018/views.py
@@ -1,63 +1,75 @@
+from datetime import datetime
 from random import shuffle
 
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from TEDx2018.models import *
+from .models import Event, TeamMember, TeamMemberAssignment, Speaker, AboutUsCarouselPhoto, getTicketsNumber
 
 
 def getTeamAndTeamMembers(event):
 	teams = event.team_set.order_by('Name')
 	teamsList = []
 	for team in teams:
-		teamMembersAssignment = list(TeamMemberAssignment.objects.filter(Team=team).only('TeamMember').order_by('TeamMember__Position', 'TeamMember__Name', 'TeamMember__Surname'))
-		teamsList.append({'Name': team.Name, 'Photo': team.Photo.url, 'PhotoDescription': team.PhotoDescription, 'TeamMembersAssignment': teamMembersAssignment})
+		teamMembersAssignment = list(
+			TeamMemberAssignment
+				.objects
+				.filter(Team=team)
+				.only('TeamMember')
+				.order_by('TeamMember__Position', 'TeamMember__Name', 'TeamMember__Surname')
+		)
+		teamsList.append({
+			'Name': team.Name,
+			'Photo': team.Photo.url,
+			'PhotoDescription': team.PhotoDescription,
+			'TeamMembersAssignment': teamMembersAssignment
+		})
 	return teamsList
 
 
-def home(request):
+def homeView(request):
 	events = Event.objects.order_by('-StartDateTime')
 	return render(request=request, template_name='TEDx2018/home.html', context={'events': events})
 
 
-def currentEvent(request):
+def currentEventView(request):
 	return render(request=request, template_name='TEDx2018/404.html')
 
 
-def upcomingEvents(request):
+def upcomingEventsView(request):
 	events = Event.objects.filter(EndDateTime__gte=datetime.now().astimezone()).order_by('-StartDateTime')
 	return render(request=request, template_name='TEDx2018/eventsListView.html', context={'events': events, 'eventType': 'Upcoming'})
 
 
-def previousEvents(request):
+def previousEventsView(request):
 	events = Event.objects.filter(EndDateTime__lte=datetime.now().astimezone()).order_by('-StartDateTime')
 	return render(request=request, template_name='TEDx2018/eventsListView.html', context={'events': events, 'eventType': 'Previous'})
 
 
-def event(request, eventName):
+def eventView(request, eventName):
 	eventName = eventName.replace('_', ' ')
 	event = Event.objects.filter(Name=eventName).first()
 	teams = getTeamAndTeamMembers(event)
 	return render(request=request, template_name='TEDx2018/event.html', context={'event': event, 'teams': teams})
 
 
-def talks(request):
+def talksView(request):
 	events = Event.objects.order_by('-StartDateTime')
 	return render(request=request, template_name='TEDx2018/talks.html', context={'events': events})
 
 
-def speakers(request):
+def speakersView(request):
 	events = Event.objects.order_by('-StartDateTime')
 	return render(request=request, template_name='TEDx2018/speakers.html', context={'events': events})
 
 
-def speakerProfile(request, fullName):
+def speakerProfileView(request, fullName):
 	name, surname = fullName.split('_')
 	speaker = Speaker.objects.filter(Name=name, Surname=surname).first()
 	return render(request=request, template_name='TEDx2018/speakerProfile.html', context={'speaker': speaker})
 
 
-def partners(request):
+def partnersView(request):
 	events = []
 	eventstmp = Event.objects.exclude(AnnouncementDateTime__gt=datetime.now().astimezone()).order_by('-StartDateTime')
 	for event in eventstmp:
@@ -73,40 +85,40 @@ def partners(request):
 	return render(request=request, template_name='TEDx2018/partners.html', context={'events': events})
 
 
-def becomeAPartner(request):
+def becomeAPartnerView(request):
 	return render(request=request, template_name='TEDx2018/becomeAPartner.html')
 
 
-def community(request):
+def communityView(request):
 	return render(request=request, template_name='TEDx2018/community.html')
 
 
-def joinAsAMember(request):
+def joinAsAMemberView(request):
 	return render(request=request, template_name='TEDx2018/joinAsAMember.html')
 
 
-def donors(request):
+def donorsView(request):
 	return render(request=request, template_name='TEDx2018/donors.html')
 
 
-def becomeADonor(request):
+def becomeADonorView(request):
 	return render(request=request, template_name='TEDx2018/becomeADonor.html')
 
 
-def about(request):
+def aboutView(request):
 	photos = list(AboutUsCarouselPhoto.objects.all())
 	shuffle(photos)
 	teams = getTeamAndTeamMembers(Event.objects.order_by('-StartDateTime').first())
 	return render(request=request, template_name='TEDx2018/about.html', context={'photos': photos, 'teams': teams})
 
 
-def teamMemberProfile(request, fullName):
+def teamMemberProfileView(request, fullName):
 	name, surname = fullName.split('_')
 	teamMember = TeamMember.objects.filter(Name=name, Surname=surname).first()
 	return render(request=request, template_name='TEDx2018/teamMemberProfile.html', context={'teamMember': teamMember})
 
 
-def ourTeam(request):
+def ourTeamView(request):
 	events = Event.objects.order_by('-StartDateTime')
 	eventsTmp = []
 	for event in events:
@@ -115,20 +127,20 @@ def ourTeam(request):
 	return render(request=request, template_name='TEDx2018/ourTeam.html', context={'events': eventsTmp})
 
 
-def contactUs(request):
+def contactUsView(request):
 	return render(request=request, template_name='TEDx2018/contactUs.html')
 
 
-def becomeAVolunteer(request):
+def becomeAVolunteerView(request):
 	return render(request=request, template_name='TEDx2018/becomeAVolunteer.html')
 
 
-def faq(request):
+def faqView(request):
 	return render(request=request, template_name='TEDx2018/faq.html')
 
 
-def custom_404(request, path):
-	return render(request=request, template_name='TEDx2018/404.html')
+def custom_404View(request, path):
+	return render(request=request, template_name='TEDx2018/404.html', context={'path': path})
 
 
 def handler404(request):
@@ -143,9 +155,5 @@ def googleConfirmation(request):
 	return render(request=request, template_name='TEDx2018/google21f2e05779fa0f76.html')
 
 
-def test(request):
-	response = requests.get('https://payment.tuc.gr/admin.php?orderid=&txid=&datefrom=&dateto=&showfrom=1', cookies=dict(PHPSESSID='aj6hfh5dhsci793cdj8pkldaj5'))
-	plainText = response.text
-	soup = BeautifulSoup(plainText)
-	trs = soup.find_all('tr', {'class': 'captured'})
-	return HttpResponse(response)
+def ticketsCounterView(request):
+	return HttpResponse(getTicketsNumber())
